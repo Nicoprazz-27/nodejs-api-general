ec1efe90312c0c1ee1c5d03276a43ab6cad1cdf9
Nicoprazz <nicolas.sirjent@groupe-esigelec.org>
2024-01-20 17:01:43 +0100

    🚚Default routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const CustomException = require('../utils/CustomException');
 
 app.use('', (req, res) =>{
-    
+    res.status(200).send('Succes');
 });
 
-
+app.use('/error', (req,res,next)=>{
+    throw new CustomException(450, 'UNKNOWN', 'Message quelconque');
+})
 
 module.exports = app;
